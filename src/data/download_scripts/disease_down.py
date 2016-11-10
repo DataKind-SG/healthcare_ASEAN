9cddb973aaf2e114865cca5ff993ffaaf9d8455d
Iwo Mateusz Dubaniowski <iwomateuszdubaniowski@Iwos-MacBook-Pro.local>
2016-11-10 11:55:09 +0800

    Added Python3 version and cosmetic changes

--- src/data/download_scripts/disease_down.py
+++ src/data/download_scripts/disease_down.py
@@ -1,13 +1,35 @@
-import urllib
-import json
-import csv
-url = 'https://data.gov.sg/api/action/datastore_search?resource_id=ef7e44f1-9b14-4680-a60a-37d2c9dda390&limit=50000'
-fileobj = urllib.urlopen(url)
-temp=json.loads(fileobj.read())
+#This script Downloads disease statistics from data.gov.sg
+
+import sys
+
+OUTFILE = "out.csv"
+DISEASE_LIST = ["Dengue Fever", "Dengue Haemorrhagic Fever", "Malaria"]
+
+if sys.version_info < (3, 0):
+    import urllib
+    import json
+    import csv
+    url = 'https://data.gov.sg/api/action/datastore_search?resource_id=ef7e44f1-9b14-4680-a60a-37d2c9dda390&limit=50000'
+    fileobj = urllib.urlopen(url)
+    temp=json.loads(fileobj.read())
 
 
-with open('out.csv', 'wb') as csvfile:
-    writethis = csv.writer(csvfile, delimiter=' ')
-    for i in temp["result"]["records"]:
-        if i["disease"]=="Dengue Fever" or i["disease"]=="Dengue Haemorrhagic Fever" or i["disease"]=="Malaria":
-            writethis.writerow([i["epi_week"], i["disease"], i["no._of_cases"]])
+    with open(OUTFILE, 'w') as csvfile:
+        writethis = csv.writer(csvfile, delimiter=' ')
+        for i in temp["result"]["records"]:
+            if i["disease"] in DISEASE_LIST:
+                writethis.writerow([i["epi_week"], i["disease"], i["no._of_cases"]])
+else:
+    import urllib.request
+    import json
+    import csv
+    url = 'https://data.gov.sg/api/action/datastore_search?resource_id=ef7e44f1-9b14-4680-a60a-37d2c9dda390&limit=50000'
+    fileobj = urllib.request.urlopen(url)
+    temp=json.loads(fileobj.read().decode("utf-8") )
+
+
+    with open(OUTFILE, 'w') as csvfile:
+        writethis = csv.writer(csvfile, delimiter=' ')
+        for i in temp["result"]["records"]:
+            if i["disease"] in DISEASE_LIST:
+                writethis.writerow([i["epi_week"], i["disease"], i["no._of_cases"]])
