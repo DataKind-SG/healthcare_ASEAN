4e5332f595438e3a440de1f50a11648f93f142c2
arynchoong <arynchoong@hotmail.com>
2017-02-13 20:35:39 +0800

    Code cleanup
    Renamed disease_down.py to SG_disease_down.py
    Define module download within SG_disease_down.py

--- src/data/download_raw_data.py
+++ src/data/download_raw_data.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
-import download_scripts.disease_down
+import download_scripts.SG_disease_down
 import download_scripts.TH_malaria_dengue
 import download_scripts.wunderground
-
+import os
 
 def validate_log_level(ctx, param, value):
     try:
@@ -15,29 +15,32 @@ def validate_log_level(ctx, param, value):
         )
     return value
 
+DIRECTORY = '../../Data/raw'
 
 @click.command()
 @click.option('--log', default='warning', callback=validate_log_level,
               help='Set logger level')
 def main(log):
+    os.makedirs(DIRECTORY, exist_ok=True)
+    
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(format=log_fmt, level=log)
     logger = logging.getLogger(__name__)
-
+    
     logger.info('Downloading raw weekly SG Dengue and Malaria data')
-    download_scripts.disease_down()
+    download_scripts.SG_disease_down.download()
     logger.info('Finished downloading raw SG data')
-
+    
     logger.info('Downloading raw TH data')
-    download_scripts.TH_malaria_dengue.download()
+#    download_scripts.TH_malaria_dengue.download()
     logger.info('Finished downloading raw TH data')
-
+    
     logger.info('Downloading wunderground data')
-    download_scripts.wunderground.download()
+#    download_scripts.wunderground.download()
     logger.info('Finished downloading wunderground data')
-
+    
     logger.info('Downloading apps.who.int data / malaria reported confirmed cases')
-    download_scripts.apps_who_int.download()
+#    download_scripts.apps_who_int.download()
     logger.info('Finished downloading apps.who.int data / malaria reported confirmed cases')
 
 if __name__ == '__main__':
