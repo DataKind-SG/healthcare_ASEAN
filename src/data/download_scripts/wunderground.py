1a8fd116d57bfcf1bc1c89d631bd2e754bc1e3c6
Le Minh Thu <leminhthu1712@gmail.com>
2017-04-24 19:48:59 +0800

    modify os.makedirs so that it works for both python2 and python3 for wunderground.py

--- src/data/download_scripts/wunderground.py
+++ src/data/download_scripts/wunderground.py
@@ -30,7 +30,14 @@ def download():
             )
             url = "https://www.wunderground.com/history/airport/{0}/{1}/1/1/CustomHistory.html?dayend=31&monthend=12&yearend={2}&format=1".format(ws, year, year)
             # and here in the folder name
-            os.makedirs(data_dir, exist_ok=True)
+             if sys.version_info < (3, 0):
+                try:
+                    os.makedirs(DIRECTORY)
+                except OSError as e:
+                    pass
+            else:   
+                os.makedirs(data_dir, exist_ok=True)
+            
             filename = os.path.join(data_dir, "{0}-{1}.csv".format(ws,year))
             #  response = urllib2.urlopen(url)
             response = urlopen(url)
